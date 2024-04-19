f0703f894a2ccb08bfe24aad68ad9300200bc9a2
Al Sayer <alsayerhaitham@gmail.com>
2024-04-19 15:11:09 +0200

    startup project

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:expense_tracker/app_view.dart';
+import 'package:flutter/material.dart';
+
+
+//here is the authentication, repositories, blocs, etc
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyAppView();
+  }
+}
