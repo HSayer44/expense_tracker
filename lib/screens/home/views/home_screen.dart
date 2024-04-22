78fdf63f2f7e62f3ab8390767b8a476dda49d8d8
Al Sayer <alsayerhaitham@gmail.com>
2024-04-22 11:04:39 +0200

    transactions chart

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,11 +1,22 @@
 import 'dart:math';
 
 import 'package:expense_tracker/screens/home/views/main_screen.dart';
+import 'package:expense_tracker/screens/stat/views/stat_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int screenIndex = 0;
+  Color selectedItemColor = Colors.blue;
+  Color unselectedItemColor = Colors.grey;
+
 
   @override
   Widget build(BuildContext context) {
@@ -14,17 +25,22 @@ class HomeScreen extends StatelessWidget {
       bottomNavigationBar: ClipRRect(
         borderRadius: BorderRadius.circular(30),
         child: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              screenIndex = value;
+            });
+          },
           backgroundColor: Colors.white,
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
-          items: const [
+          items: [
             BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home),
+              icon: Icon(CupertinoIcons.home, color: screenIndex == 0 ? selectedItemColor : unselectedItemColor),
               label: 'home',
             ),
             BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.graph_square_fill),
+              icon: Icon(CupertinoIcons.graph_square_fill, color: screenIndex == 1 ? selectedItemColor : unselectedItemColor),
               label: 'Stats',
             ),
           ],
@@ -50,7 +66,7 @@ class HomeScreen extends StatelessWidget {
             child: const Icon(CupertinoIcons.add)),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: const MainScreen(),
+      body: screenIndex == 0 ? MainScreen() : StatScreen(),
     );
   }
 }
