bc2d463a881a08dc3e4b38d7976d2b0c20c31dda
Al Sayer <alsayerhaitham@gmail.com>
2024-04-22 09:12:23 +0200

    main screen design

--- lib/screens/home/views/main_screen.dart
+++ lib/screens/home/views/main_screen.dart
@@ -1,5 +1,10 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:expense_tracker/data/data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -20,9 +25,12 @@ class MainScreen extends StatelessWidget {
                     Container(
                       width: 50,
                       height: 50,
-                      decoration:  BoxDecoration(shape: BoxShape.circle, color: Colors.yellow[700]),
+                      decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.yellow[700]),
                     ),
-                    Icon(CupertinoIcons.person_fill, color: Colors.yellow[900],)
+                    Icon(
+                      CupertinoIcons.person_fill,
+                      color: Colors.yellow[900],
+                    )
                   ],
                 ),
                 const SizedBox(width: 8),
@@ -40,10 +48,207 @@ class MainScreen extends StatelessWidget {
                   ],
                 ),
                 const Expanded(child: SizedBox()),
-                IconButton(onPressed: () {
-                  
-                }, icon: const Icon(CupertinoIcons.settings))
+                IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.settings))
               ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 2,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 2,
+                    color: Colors.grey.shade300,
+                    offset: const Offset(5, 5),
+                  ),
+                ],
+                gradient: LinearGradient(
+                  transform: const GradientRotation(pi / 4),
+                  colors: [
+                    colorTheme.primary,
+                    colorTheme.secondary,
+                    colorTheme.tertiary,
+                  ],
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Total Balance',
+                    style: TextStyle(fontSize: 16, color: Colors.white, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(height: 12),
+                  const Text(
+                    '€ 4800.00',
+                    style: TextStyle(fontSize: 35, color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 12),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              width: 25,
+                              height: 25,
+                              decoration: const BoxDecoration(
+                                color: Colors.white30,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  CupertinoIcons.arrow_down,
+                                  size: 12,
+                                  color: Colors.greenAccent,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Income',
+                                  style: TextStyle(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w400),
+                                ),
+                                Text(
+                                  '€ 2500.00',
+                                  style: TextStyle(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              width: 25,
+                              height: 25,
+                              decoration: const BoxDecoration(
+                                color: Colors.white30,
+                                shape: BoxShape.circle,
+                              ),
+                              child: const Center(
+                                child: Icon(
+                                  CupertinoIcons.arrow_up,
+                                  size: 12,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            const Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Expenses',
+                                  style: TextStyle(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w400),
+                                ),
+                                Text(
+                                  '€ 800.00',
+                                  style: TextStyle(fontSize: 14, color: Colors.white, fontWeight: FontWeight.w600),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Transactions',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: colorTheme.onBackground,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'View All',
+                    style: TextStyle(
+                      color: colorTheme.outline,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: transactionsData.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 12.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 50,
+                              height: 50,
+                              decoration: BoxDecoration(
+                                color: transactionsData[index]['color'],
+                                shape: BoxShape.circle,
+                              ),
+                              child: Center(
+                                  child: transactionsData[index]['icon'],
+                              ),
+                            ),
+                            const SizedBox(width: 12),
+                            Text(
+                              transactionsData[index]['name'],
+                              style: TextStyle(
+                                color: colorTheme.onBackground,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            const Expanded(child: SizedBox()),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                Text(
+                                  transactionsData[index]['totalAmount'],
+                                  style: TextStyle(
+                                    color: colorTheme.onBackground,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                Text(
+                                  transactionsData[index]['date'],
+                                  style: TextStyle(
+                                    color: colorTheme.outline,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             )
           ],
         ),
